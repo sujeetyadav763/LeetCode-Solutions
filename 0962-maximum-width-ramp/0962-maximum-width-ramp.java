cc623a1a26145bca123366f6e7398f1191aea542
Sujeet Yadav <yadavsuzeet763@gmail.com>
2024-10-10 16:02:23 +0530

    Time: 3 ms (97.76%), Space: 55.1 MB (27.61%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        int[] rightMax = new int[n];
+
+        // Fill rightMax array with the maximum values from the right
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        }
+
+        int left = 0, right = 0;
+        int maxWidth = 0;
+
+        // Traverse the array using left and right pointers
+        while (right < n) {
+            // Move left pointer forward if current left exceeds rightMax
+            while (left < right && nums[left] > rightMax[right]) {
+                left++;
+            }
+            maxWidth = Math.max(maxWidth, right - left);
+            right++;
+        }
+
+        return maxWidth;
+    }
+}
